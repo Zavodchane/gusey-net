2416809f5fd46ed388c93e8ea8358ace9d601f14
arcanegolem <dopeflipper@gmail.com>
2023-05-18 00:09:56 +0300

    добавлена модель + ui импрувнут

--- windows/MainWindow.py
+++ windows/MainWindow.py
@@ -9,10 +9,14 @@ from PyQt6.QtWidgets import (
     QScrollArea,
     QTextBrowser,
     QLabel,
-    QSlider
+    QSlider,
+    QPushButton,
+    QCheckBox,
+    QLineEdit,
+    QFileDialog
 )
 
-from PyQt6.QtGui import QIcon
+from PyQt6.QtGui import QIcon, QMovie
 from PyQt6.QtCore import Qt
 
 import os
@@ -20,11 +24,20 @@ import os
 from constants.paths import RESULT_PATH
 from widgets.ImageBrowser import ImageBrowser
 
+from yolov5.detect import detect
+
 from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
 from matplotlib.backends.backend_qt5agg import NavigationToolbar2QT as NavigationTool
 from matplotlib.figure import Figure
 
-def loadPaths(startpath: str, tree) -> None:
+from pathlib import WindowsPath
+
+from torch import cuda
+
+from threading import Thread
+
+
+def loadPaths(startpath: str, tree : QTreeWidget) -> None:
     for elem in os.listdir(startpath):
         elemPath = startpath + "/" + elem
         parentalItem = QTreeWidgetItem(tree, [os.path.basename(elem)])
@@ -48,6 +61,8 @@ def getItemFullPath(item : QTreeWidgetItem) -> str:
 
 
 class MainWindow(QMainWindow):
+    currentlySelectedFolder = ""
+
     def __init__(self) -> None:
         super().__init__()
 
@@ -104,35 +119,139 @@ class MainWindow(QMainWindow):
 
         loadPaths(startpath=RESULT_PATH, tree=self.resultFileTree)
 
-        self.sliderLayout = QHBoxLayout()
+        self.detectionPercentSliderLayout = QHBoxLayout()
 
-        self.detectionSliderLabel = QLabel("Процент уверенности")
+        self.detectionSliderLabel = QLabel("Минимальный процент уверенности")
         self.detectionSliderLabel.setMaximumHeight(15)
         self.detectionSliderLabel.setAlignment(Qt.AlignmentFlag.AlignHCenter)
 
-        self.detectionPercentLabel = QLabel("1%")
+        self.detectionPercentLabel = QLabel("50%")
         self.detectionPercentLabel.setMaximumWidth(30)
         self.detectionPercentLabel.setAlignment(Qt.AlignmentFlag.AlignRight)
 
         self.detectionPercentSlider = QSlider(Qt.Orientation.Horizontal)
         self.detectionPercentSlider.setMaximumWidth(int(self.windowWidth / 5) - 35)
         self.detectionPercentSlider.setRange(1, 100)
+        self.detectionPercentSlider.setValue(50)
         self.detectionPercentSlider.setSingleStep(1)
-        self.detectionPercentSlider.valueChanged.connect(self.onSliderValueChange)
+        self.detectionPercentSlider.valueChanged.connect(self.onPercentSliderValueChange)
 
-        self.sliderLayout.addWidget(self.detectionPercentLabel)
-        self.sliderLayout.addWidget(self.detectionPercentSlider)
+        self.detectionPercentSliderLayout.addWidget(self.detectionPercentLabel)
+        self.detectionPercentSliderLayout.addWidget(self.detectionPercentSlider)
+
+
+        self.lineThicknessSliderLayout = QHBoxLayout()
+
+        self.lineThicknessSliderLabel = QLabel("Толщина линий выделения")
+        self.lineThicknessSliderLabel.setMaximumHeight(15)
+        self.lineThicknessSliderLabel.setAlignment(Qt.AlignmentFlag.AlignHCenter)
+
+        self.lineThicknessLabel = QLabel("3")
+        self.lineThicknessLabel.setMaximumWidth(30)
+        self.lineThicknessLabel.setAlignment(Qt.AlignmentFlag.AlignRight)
+
+        self.lineThicknessSlider = QSlider(Qt.Orientation.Horizontal)
+        self.lineThicknessSlider.setMaximumWidth(int(self.windowWidth / 5) - 35)
+        self.lineThicknessSlider.setRange(1, 10)
+        self.lineThicknessSlider.setValue(3)
+        self.lineThicknessSlider.setSingleStep(1)
+        self.lineThicknessSlider.valueChanged.connect(self.onThicknessSliderValueChange)
+
+        self.lineThicknessSliderLayout.addWidget(self.lineThicknessLabel)
+        self.lineThicknessSliderLayout.addWidget(self.lineThicknessSlider)
+
+
+        self.deviceCheckBoxesLabel = QLabel("Девайс")
+        self.deviceCheckBoxesLabel.setMaximumHeight(15)
+        self.deviceCheckBoxesLabel.setAlignment(Qt.AlignmentFlag.AlignHCenter)
+
+        self.deviceCheckBoxesLayout = QHBoxLayout()
+
+        self.cpuCheckBox  = QCheckBox("CPU")
+        self.cpuCheckBox.setMaximumWidth(50)
+        self.cpuCheckBox.setChecked(True)
+        self.cpuCheckBox.stateChanged.connect(self.checkBoxCheckMate)
+
+        self.cudaCheckBox = QCheckBox("GPU") 
+        self.cudaCheckBox.setEnabled(cuda.is_available())
+        self.cudaCheckBox.stateChanged.connect(self.checkBoxCheckMate)
+        self.cudaCheckBox.setMaximumWidth(50)
+
+        self.deviceCheckBoxesLayout.addWidget(self.cpuCheckBox)
+        self.deviceCheckBoxesLayout.addWidget(self.cudaCheckBox)
+
+
+        self.pathLayout = QHBoxLayout()
+
+        self.pathLabel = QLabel("Выбранная папка:")
+        self.pathLabel.setMaximumHeight(15)
+        self.pathLabel.setAlignment(Qt.AlignmentFlag.AlignLeft)
+        self.pathLabel.setMaximumWidth(100)
+
+        self.pathDisplay = QLineEdit(self.currentlySelectedFolder)
+        self.pathDisplay.setMaximumWidth(int(self.windowWidth / 5) - 100)
+        self.pathDisplay.setReadOnly(True)
+
+        self.pathLayout.addWidget(self.pathLabel)
+        self.pathLayout.addWidget(self.pathDisplay)
+
+
+        self.chooseFolderButton = QPushButton("Выбрать папку")
+        self.chooseFolderButton.clicked.connect(self.chooseFolder)
+
+
+        self.detectButton = QPushButton("Detect")
+        self.detectButton.clicked.connect(self.onDetectButtonClicked)
+        self.detectButton.setEnabled(False)
+
 
         self.firstColLayout.addWidget(self.resultFileTree)
         self.firstColLayout.addWidget(self.detectionSliderLabel)
-        self.firstColLayout.addLayout(self.sliderLayout)
+        self.firstColLayout.addLayout(self.detectionPercentSliderLayout)
+        self.firstColLayout.addWidget(self.lineThicknessSliderLabel)
+        self.firstColLayout.addLayout(self.lineThicknessSliderLayout)
+        self.firstColLayout.addWidget(self.deviceCheckBoxesLabel)
+        self.firstColLayout.addLayout(self.deviceCheckBoxesLayout)
+        self.firstColLayout.addLayout(self.pathLayout)
+        self.firstColLayout.addWidget(self.chooseFolderButton)
+        self.firstColLayout.addWidget(self.detectButton)
 
 
-    def onSliderValueChange(self):
+    def onPercentSliderValueChange(self):
         '''
         Функция вызываемая при смене значения на слайдере
         '''
         self.detectionPercentLabel.setText(str(self.sender().value()) + "%")
+
+    
+    def onThicknessSliderValueChange(self):
+        '''
+        Функция вызываемая при смене значения на слайдере
+        '''
+        self.lineThicknessLabel.setText(str(self.sender().value()))
+
+
+    def checkBoxCheckMate(self):
+        if self.sender().checkState() == Qt.CheckState.Checked:
+            if   self.sender() == self.cpuCheckBox:
+                self.cudaCheckBox.setChecked(False)
+            elif self.sender() == self.cudaCheckBox:
+                self.cpuCheckBox.setChecked(False)
+
+
+    def chooseFolder(self):
+        currentUser = os.environ.get('USER', os.environ.get('USERNAME'))
+        self.currentlySelectedFolder = QFileDialog(directory=f"C:/Users/{currentUser}/Pictures").getExistingDirectory()
+
+        self.updatePathDisplay()
+
+
+    def updatePathDisplay(self):
+        self.pathDisplay.setText(self.currentlySelectedFolder)
+        if self.currentlySelectedFolder != "":
+            self.detectButton.setEnabled(True)
+        else:
+            self.detectButton.setEnabled(False)
 
 
     def initImageBrowser(self):
@@ -206,13 +325,62 @@ class MainWindow(QMainWindow):
 
         self.canvas.draw()
 
+    
+    def onDetectButtonClicked(self):
+        detection_thread = Thread(target=self.detect)
+        detection_thread.start()
+
+    
+    def detect(self):
+        self.detectButton.setEnabled(False)
+
+        if self.cudaCheckBox.isChecked():
+            device = "cuda:0"
+        else: 
+            device = "cpu"
+
+        options = {
+            'weights': ['D:\\FrequentUsageFolder\\Coding\\Python\\SwanHackUI\\yolov5\\runs\\train\\exp14\\weights\\best.pt'], 
+            'source': self.currentlySelectedFolder, 
+            'data': WindowsPath('data/coco128.yaml'), 
+            'imgsz': [640, 640], 
+            'conf_thres': self.detectionPercentSlider.value()/100, 
+            'iou_thres': 0.45, 
+            'max_det': 1000, 
+            'device': device, 
+            'view_img': False, 
+            'save_txt': False, 
+            'save_conf': False, 
+            'save_crop': False, 
+            'nosave': False, 
+            'classes': None, 
+            'agnostic_nms': False, 
+            'augment': False, 
+            'visualize': False, 
+            'update': False, 
+            'project': WindowsPath('results'), 
+            'name': 'run', 
+            'exist_ok': False, 
+            'line_thickness': self.lineThicknessSlider.value(), 
+            'hide_labels': False, 
+            'hide_conf': False, 
+            'half': False, 
+            'dnn': False, 
+            'vid_stride': 1
+        }
+        
+        detect(options)
+
+        self.resultFileTree.clear()
+        loadPaths(RESULT_PATH, self.resultFileTree)
+
+        self.currentlySelectedFolder = ""
+        self.updatePathDisplay()
+
 
     def treeItemClicked(self, it : QTreeWidgetItem, col):
         '''
         Функция вызываемая при нажатии на элемент в дереве файлов папки результатов
         '''
         if not os.path.isdir(getItemFullPath(it)):
-            print(getItemFullPath(it))
             self.imageBrowser.updatePixmap(getItemFullPath(it))
-        else:
-            print("Its a folder!")
